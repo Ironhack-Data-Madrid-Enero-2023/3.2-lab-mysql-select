3d60bd9f3b34dbe95c09b03eff6b09aa96134dac
Paula Alvarez <paula.alvarez.sanchezamoraga@gmail.com>
2023-01-31 23:02:11 +0100

    challenge1y2

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,5 +1,6 @@
 SELECT * from sales;
 
+#challengue 1
 select authors.au_id as 'AUTHOR_ID', authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
 	from authors 
     
@@ -12,3 +13,22 @@ select authors.au_id as 'AUTHOR_ID', authors.au_lname, authors.au_fname, titles.
     left join publishers
     on publishers.pub_id=titles.pub_id
     ;
+
+#challenge 2
+    
+
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+ publishers.pub_name AS "PUBLISHER", count(titles.title_id) AS "TITLE COUNT"
+ 
+ FROM authors
+ 
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+
+    GROUP BY authors.au_id, publishers.pub_name
+    ;
+	
+    
+
+    
