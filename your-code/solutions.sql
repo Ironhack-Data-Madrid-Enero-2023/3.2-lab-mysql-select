fd3ade06a22b2bca2499da738fe80f41824464a7
Ruben <ruben.sego@gmail.com>
2023-01-31 17:45:06 +0100

    uno

---
+++ your-code/solutions.sql
@@ -0,0 +1,46 @@
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME",au_fname as "FIRST NAME" , titles.title as "TITLE", publishers.pub_name as "PUBLISHER"
+from authors
+left join titleauthor
+on authors.au_id = titleauthor.au_id
+left join titles
+on titleauthor.title_id = titles.title_id
+left join publishers
+on titles.pub_id = publishers.pub_id
+;
+
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME",au_fname as "FIRST NAME" , publishers.pub_name as "PUBLISHER", count(titleauthor.title_id) as "TITLE COUNT"
+from authors
+left join titleauthor
+on authors.au_id = titleauthor.au_id
+left join titles
+on titleauthor.title_id = titles.title_id
+left join publishers
+on titles.pub_id = publishers.pub_id
+group by authors.au_id, publishers.pub_name
+;
+
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME",au_fname as "FIRST NAME", sales.qty as "TOTAL"
+from authors
+left join titleauthor
+on authors.au_id = titleauthor.au_id
+left join titles
+on titleauthor.title_id = titles.title_id
+left join sales
+on titles.title_id = sales.title_id
+order by sales.qty desc
+limit 3
+;
+
+
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME",au_fname as "FIRST NAME", coalesce(sum(sales.qty), 0) as "TOTAL"
+from authors
+left join titleauthor
+on authors.au_id = titleauthor.au_id
+left join titles
+on titleauthor.title_id = titles.title_id
+left join sales
+on titles.title_id = sales.title_id
+group by authors.au_id, sales.qty
+order by sales.qty desc
+limit 23
+;
